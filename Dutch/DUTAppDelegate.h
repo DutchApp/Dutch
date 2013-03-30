7af95e26cd4210a25f7f0eaa4d171c492d6b0284
rajmohan lokanath <rajmirage@MyMBP.local>
2013-03-30 12:44:38 -0700

    Initial Commit

---
+++ Dutch/DUTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DUTAppDelegate.h
+//  Dutch
+//
+//  Created by rajmohan lokanath on 3/30/13.
+//  Copyright (c) 2013 Dutch Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DUTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Dutch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dutch
+//
+//  Created by rajmohan lokanath on 3/30/13.
+//  Copyright (c) 2013 Dutch Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DUTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DUTAppDelegate class]));
+    }
+}
