f6ff176c0edcb9223453b26f99dc4bdbaa44c996
rajmohan lokanath <rajmirage@MyMBP.local>
2013-04-07 17:46:00 -0700

    deleted ui
     Please enter the commit message for your changes. Lines starting

---
+++ Dutch/Utility/DUTUtility+Validation.h
@@ -0,0 +1,13 @@
+//
+//  DUTUtility+Validation.h
+//  Dutch
+//
+//  Created by rajmohan lokanath on 4/7/13.
+//  Copyright (c) 2013 Dutch Inc. All rights reserved.
+//
+
+#import "DUTUtility.h"
+
+@interface DUTUtility (Validation)
+
+@end
---
+++ Dutch/Utility/DUTUtility.m
@@ -0,0 +1,13 @@
+//
+//  DUTUtility.m
+//  Dutch
+//
+//  Created by rajmohan lokanath on 4/7/13.
+//  Copyright (c) 2013 Dutch Inc. All rights reserved.
+//
+
+#import "DUTUtility.h"
+
+@implementation DUTUtility
+
+@end
