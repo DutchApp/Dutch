f1518ec0da1fda0f45f62ecd41e4be95ba803e22
anuj Chaudhary <anuj1604@gmail.com>
2013-07-13 14:02:35 -0700

    Main Menu Changes

--- Dutch/ViewControllers/DUTMenuViewController.h
+++ Dutch/ViewControllers/DUTMenuViewController.h
@@ -6,7 +6,19 @@
 //  Copyright (c) 2013 Dutch Inc. All rights reserved.
 //
 
+
+// *************************************************************************************************
+#pragma mark -
+#pragma mark Imports
+
+
 #import <UIKit/UIKit.h>
+
+
+// *************************************************************************************************
+#pragma mark -
+#pragma mark Interface
+
 
 @interface DUTMenuViewController : UIViewController
 
--- Dutch/ViewControllers/DUTMenuViewController.m
+++ Dutch/ViewControllers/DUTMenuViewController.m
@@ -6,17 +6,41 @@
 //  Copyright (c) 2013 Dutch Inc. All rights reserved.
 //
 
+
+// *************************************************************************************************
+#pragma mark -
+#pragma mark Imports
+
+
 #import "DUTMenuViewController.h"
+
+
+// *************************************************************************************************
+#pragma mark -
+#pragma mark Private Interface
 
 
 @interface DUTMenuViewController ()
 
-@property(nonatomic,strong,readwrite) DUTGroupedCellControllerContainer *controllerContainer;
+
+@property (nonatomic, strong, readwrite) DUTGroupedCellControllerContainer *controllerContainer;
+
 
 @end
 
 
+// *************************************************************************************************
+#pragma mark -
+#pragma mark Implementation
+
+
 @implementation DUTMenuViewController
+
+
+// *************************************************************************************************
+#pragma mark -
+#pragma mark View Controller Overrides
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -37,6 +61,11 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+// *************************************************************************************************
+#pragma mark -
+#pragma mark Private Methods
 
 
 - (void)setupSections {
